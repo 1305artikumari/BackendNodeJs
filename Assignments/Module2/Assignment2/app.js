d0d55f4e76e402a46f382b02cb395ff72cd18d7e
Arti Kumari <107554479+1305artikumari@users.noreply.github.com>
2023-10-03 21:20:20 +0530

    Create app.js

---
+++ Assignments/Module2/Assignment2/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/home', (req, res, next) => {
+    res.send('<form action="/message" method="POST"><input type="text" name="titleName"><input type="text" name="titleValue"><button type="submit">Send</button></form>');
+})
+app.use('/message', (req, res, next) => {
+    console.log(`${req.body.titleName} : ${req.body.titleValue}`);
+    res.redirect('back');
+})
+
+
+app.listen(4004);
