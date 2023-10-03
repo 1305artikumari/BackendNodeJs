b59684ee58a5591e0e87e1f9717fc2fc70a561b8
Arti Kumari <107554479+1305artikumari@users.noreply.github.com>
2023-10-03 21:21:47 +0530

    Update app.js

--- Assignments/Module2/Assignment2/app.js
+++ Assignments/Module2/Assignment2/app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/home', (req, res, next) => {
-    res.send('<form action="/message" method="POST"><input type="text" name="titleName"><input type="text" name="titleValue"><button type="submit">Send</button></form>');
-})
-app.use('/message', (req, res, next) => {
-    console.log(`${req.body.titleName} : ${req.body.titleValue}`);
-    res.redirect('back');
-})
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req, res , next) =>{
+    res.send('<form action="/Product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    
+});
+
+app.post('/product', (req, res , next) => {
+    console.log(req.body);
+   res.redirect('/');
+});
 
 
-app.listen(4004);
+app.use('/',(req, res , next) =>{
+   res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(4003);
